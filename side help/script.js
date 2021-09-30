d1f59b7bec1dca7bbaadf6cbb17a5604e174ac6a
AliEmadEldin <aly.emad79@gmail.com>
2021-10-01 00:03:07 +0200

    first commit

---
+++ side help/script.js
@@ -0,0 +1,18 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        300:{
+            items:3
+        },
+        600:{
+            items:2
+        }
+    }
+    
+})
